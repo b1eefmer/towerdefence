52fabfe9334e0a4802ab38b462a91c92fb3700cc
b1eefmer <yevheniilukianenko@gmail.com>
2026-03-25 16:21:27 +0100

    add waves, turret

---
+++ Assets/Turret.cs
@@ -0,0 +1,78 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("References")]
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+
+
+    [Header("Attributes")]
+    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationSpeed = 200f;
+    [SerializeField] private float bps = 1f; //BULLETS PER SECOND
+
+    private Transform target;
+    private float timeUntilFire;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update() 
+    {
+        if (target == null) 
+        {
+            FindTarget();
+            return;
+        }
+        RotateTowardsTarget();
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else
+        {
+            timeUntilFire += Time.deltaTime;
+            if (timeUntilFire >= 1f / bps)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+    }
+    private void Shoot()
+    {
+        Debug.Log("Shoot");
+    }
+    private void FindTarget () 
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+    private void RotateTowardsTarget() 
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed*Time.deltaTime);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+}
