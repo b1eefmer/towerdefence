4c6c799d3453d893699c16bb5743d2607f255459
nataliasuda <natalia.suda7@wp.pl>
2026-05-02 02:07:20 +0200

    feat: add OptionsScene and implement in button

--- Assets/Code/Scripts/MainMenu.cs
+++ Assets/Code/Scripts/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void Options()
     {
-        Debug.Log("Options clicked");
+        SceneManager.LoadScene("OptionsScene");
     }
 
     public void QuitGame()
